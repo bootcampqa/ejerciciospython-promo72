2d7492691e79b253ea2a2810dcceed4f5e36cf18
Reyes Cuesta Bootcamp QA <mybootcampqa@gmail.com>
2026-06-09 18:57:24 +0200

    ejercicio variables

---
+++ ejercicios/t1e1peliculas.py
@@ -0,0 +1,26 @@
+titulo_de_la_pelicula = "Harry Potter y la Piedra Filosofal" 
+direccion_de_la_pelicula = "Chris Columbus"
+fecha_de_lanzamiento = 2001
+genero = "Fantasia"
+duracion_en_minutos = 152
+tiene_premios= True
+
+
+print("Nombre:", titulo_de_la_pelicula)
+print("Dirección:", direccion_de_la_pelicula)
+print("Estreno:", fecha_de_lanzamiento)
+print("Género:", genero)
+print("Duración:", duracion_en_minutos)
+print("Premios:", tiene_premios)
+
+titulo_de_la_pelicula = "Harry Potter and the Philosopher's Stone" 
+genero = "Fantasy"
+
+
+print("Nombre" , titulo_de_la_pelicula)
+print("Direccion" , direccion_de_la_pelicula)
+print("Estreno" , fecha_de_lanzamiento)
+print("Género" , genero)
+print("Duración" , duracion_en_minutos)
+print("Premios" , tiene_premios)
+
