174d8ebffb56cca666648f6760203d0a75981568
Reyes Cuesta Bootcamp QA <mybootcampqa@gmail.com>
2026-06-09 19:52:59 +0200

    ejercicio correo cadenas

---
+++ ejercicios/t2e4correo.py
@@ -0,0 +1,14 @@
+# Pide a la usuaria un correo electronico
+correo_electronico = input("Ingresa un correo electrónico: ")
+
+# longitud del correo electronico
+longitud = len(correo_electronico)
+print(longitud)
+
+# Correo en mayusculas
+mayusculas = correo_electronico.upper ()
+print(mayusculas)
+
+# Correo en minusculas
+minusculas = correo_electronico.lower ()
+print(minusculas)
